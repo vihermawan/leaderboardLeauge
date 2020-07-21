9d610ede9324b796561d9b0ea8bece8fd03a81cd
vicky hermawan <vickyhermawan99@mail.ugm.ac.id>
2020-07-21 19:52:28 +0700

    get data belum selesai, tambah loading

--- src/app/standing-page/liga-page/premier-page.js
+++ src/app/standing-page/liga-page/premier-page.js
@@ -3,42 +3,75 @@ import PremierComponent from '../../../modules/standing-component/liga-component
 
 class PremierPage extends Component { 
     state = {
-
+        standing : [],
+        tanggal_mulai : '',
+        tanggal_selesai : '',
+        loading: false,
     }
+
+    componentDidMount(){
+        this.getData();
+
+        // this.setState({
+        //     tanggal_mulai : json.season.startDate,
+        //     tanggal_selesai : json.season.endDate,
+        // })
+    }
+
+    getData = async () =>{
+        let headers = {
+            'X-Auth-Token' : '2d8c0efa2cca4b2db7e51f3265d872be'
+        };
+
+        let config = {
+            headers : headers,
+        }
+        this.setState({loading : true})
+        const response = await fetch('https://api.football-data.org/v2/competitions/2021/standings?standingType=HOME',config)
+       
+        if( response.status === 200) {
+            const json = await response.json();
+            console.log('data', response)
+            this.setState({loading : false})
+        }
+        
+    }
+
+
     render(){
 
         const data = [
             {
-              key: '1',
-              name: 'Mike',
-              age: 32,
-              address: '10 Downing Street',
+                key: '1',
+                name: 'Mike',
+                age: 32,
+                address: '10 Downing Street',
             },
             {
-              key: '2',
-              name: 'John',
-              age: 42,
-              address: '10 Downing Street',
+                key: '2',
+                name: 'John',
+                age: 42,
+                address: '10 Downing Street',
             },
-          ];
+        ];
           
-          const columns = [
+        const columns = [
             {
-              title: 'Name',
-              dataIndex: 'name',
-              key: 'name',
+                title: 'Name',
+                dataIndex: 'name',
+                key: 'name',
             },
             {
-              title: 'Age',
-              dataIndex: 'age',
-              key: 'age',
+                title: 'Age',
+                dataIndex: 'age',
+                key: 'age',
             },
             {
-              title: 'Address',
-              dataIndex: 'address',
-              key: 'address',
+                title: 'Address',
+                dataIndex: 'address',
+                key: 'address',
             },
-          ];
+        ];
 
         return (
             <PremierComponent
--- src/modules/standing-component/liga-component/premier-component.js
+++ src/modules/standing-component/liga-component/premier-component.js
@@ -1,31 +1,36 @@
 import React, { Component } from 'react';
 import { Layout, Row, Col, Table} from 'antd';
+import LoadingContainer from '../../../common/component/loading/loadingContainer';
 
 class PremierComponent extends Component {
     state = {  }
     render() { 
         const { Content } = Layout;
-        const {data, columns} = this.props;
+        const {data, columns, initialData} = this.props;
         return ( 
             <Layout>
             <Content className="dashboard-section-content background-white">
-                <Row>
-                    <Col lg={24} md={24} sm={24}>
-                        <span className="font-bold font-title-content black">Tabel Klasemen Liga Inggris</span>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col lg={24} md={24} sm={24} className="mt-20">
-                        <div className="dashboard-section-table-dashboard">
-                            <Table
-                                columns={columns} 
-                                dataSource={data} 
-                                className="table-dashboard"
-                                scroll={{ x: 1800 }}
-                            />
-                        </div>
-                    </Col>
-                </Row>
+                <LoadingContainer loading={initialData.loading}>
+                    <Row>
+                        <Col lg={24} md={24} sm={24}>
+                            <span className="font-bold font-title-content black">Tabel Klasemen Liga Inggris</span>
+                            <br/>
+                            <span>{initialData.tanggal_mulai}</span>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={24} md={24} sm={24} className="mt-20">
+                            <div className="dashboard-section-table-dashboard">
+                                <Table
+                                    columns={columns} 
+                                    dataSource={data} 
+                                    className="table-dashboard"
+                                    scroll={{ x: 1800 }}
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+                </LoadingContainer>
             </Content>
         </Layout>
         );
